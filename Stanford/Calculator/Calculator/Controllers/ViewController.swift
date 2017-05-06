eacf27ffaee388968773745b1f5356fb6ef2571a
Hans Kammler <coolmacmaniac@gmail.com>
2017-05-06 18:16:11 +0530

    Refined ui colours and fonts

--- Stanford/Calculator/Calculator/Controllers/ViewController.swift
+++ Stanford/Calculator/Calculator/Controllers/ViewController.swift
@@ -12,9 +12,17 @@ class ViewController: UIViewController {
 	
 	@IBOutlet private weak var display: UILabel!
 	
+	@IBOutlet private weak var squareButton: UIButton!
+	
+	@IBOutlet weak var orientationStack: UIStackView!
+	
+	@IBOutlet weak var mathematicalStack: UIStackView!
+	
 	private var userIsInTheMiddleOfTyping = false
 	
 	private var brain = CalculatorBrain()
+	
+	private var savedProgram: CalculatorBrain.Instructions?
 	
 	private var displayValue: Double {
 		get {
@@ -41,6 +49,39 @@ class ViewController: UIViewController {
 		return displayString.contains(".")
 	}
 	
+	override func viewWillAppear(_ animated: Bool) {
+		// customise display font to digital font
+		display.font = UIFont(name: "DBLCDTempBlack", size: 40.0);
+		
+		// customise superscript for square button
+		let mainFont = UIFont.systemFont(ofSize: 25.0)
+		let superFont = UIFont.systemFont(ofSize: 12.0)
+		//let color = UIColor(colorLiteralRed: 6.0/255.0, green: 76.0/255.0, blue: 150.0/255.0, alpha: 1.0)
+		let color = UIColor.white
+		let squareStr = NSMutableAttributedString(string: (squareButton.titleLabel?.text)!, attributes: [NSFontAttributeName: mainFont, NSForegroundColorAttributeName: color])
+		squareStr.setAttributes([NSFontAttributeName: superFont, NSBaselineOffsetAttributeName: 10, NSForegroundColorAttributeName: color], range:NSRange(location: 1, length: 1))
+		squareButton.setAttributedTitle(squareStr, for: .normal)
+		
+		// customise round corners for display
+		display.layer.cornerRadius = 5.0
+		display.clipsToBounds = true
+		
+	}
+	
+	override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+		if UIDevice.current.orientation.isLandscape {
+			// rotating to landscape orientation
+			orientationStack.axis = .horizontal
+			mathematicalStack.axis = .vertical
+			orientationStack.insertArrangedSubview(mathematicalStack, at: orientationStack.subviews.endIndex)
+		} else {
+			// rotating to portrait orientation
+			orientationStack.axis = .vertical
+			mathematicalStack.axis = .horizontal
+			orientationStack.insertArrangedSubview(mathematicalStack, at: orientationStack.subviews.startIndex)
+		}
+	}
+	
 	@IBAction private func touchDigit(_ sender: UIButton) {
 		let digit = sender.currentTitle!
 		if userIsInTheMiddleOfTyping {
@@ -53,7 +94,7 @@ class ViewController: UIViewController {
 		}
 	}
 	
-	@IBAction func performOperation(_ sender: UIButton) {
+	@IBAction private func performOperation(_ sender: UIButton) {
 		userIsInTheMiddleOfTyping = false
 		if let symbol = sender.currentTitle {
 			brain.setOperand(displayValue)
@@ -62,13 +103,42 @@ class ViewController: UIViewController {
 		}
 	}
 	
-	@IBAction func touchDot(_ sender: UIButton) {
+	@IBAction private func touchDot(_ sender: UIButton) {
 		guard !displayStringHasDot else {
 			return
 		}
 		let digit = sender.currentTitle!
 		displayString = displayString + digit
 		userIsInTheMiddleOfTyping = true
+	}
+	
+	@IBAction func doSaveCalculations() {
+		
+		// save brains state here & clear from brain
+		savedProgram = brain.program
+	}
+	
+	@IBAction func doPlayCalaulations() {
+		
+		// load state from here into brain & start playing
+		if let savedProgram = savedProgram {
+			brain.program = savedProgram
+			
+			// update the result on UI
+			displayValue = brain.result
+		}
+	}
+	
+	@IBAction func doClearCalaulations() {
+		
+		// clear the brain
+		brain.clearAll()
+		
+		// clear the cache
+		savedProgram = nil
+		
+		// clear the display
+		displayValue = brain.result
 	}
 }
 
--- Stanford/Calculator/Calculator/Models/CalculatorBrain.swift
+++ Stanford/Calculator/Calculator/Models/CalculatorBrain.swift
@@ -16,16 +16,20 @@ public class CalculatorBrain {
 	
 	private var pending: PendingBinaryOperationInfo?
 	
+	private var currentProgram = [Instructions]()
+	
 	private let operation: [String: Operation] = [
 		"π": .Constant(Double.pi),
 		"e": .Constant(M_E),
 		"√": .UnaryOperation(sqrt),
 		"±": .UnaryOperation({ -$0 }),
+		"%": .UnaryOperation({ $0 / 100 }),
+		"sq": .UnaryOperation({ $0 * $0 }),
 		"×": .BinaryOperation({ $0 * $1 }),
 		"÷": .BinaryOperation({ $0 / $1 }),
 		"+": .BinaryOperation({ $0 + $1 }),
 		"−": .BinaryOperation({ $0 - $1 }),
-		"=": .Equals
+		"=": .Equals,
 	]
 	
 	private enum Operation {
@@ -49,25 +53,56 @@ public class CalculatorBrain {
 	
 	//MARK: - public members
 	
-	public func setOperand(_ operand: Double) {
-		accumulator = operand
-	}
+	public typealias Instructions = AnyObject
 	
-	public func doOperation(_ symbol: String) {
-		switch operation[symbol]! {
-		case .Constant(let value): accumulator = value
-		case .UnaryOperation(let function): accumulator = function(accumulator)
-		case .BinaryOperation(let function):
-			executePendingBinaryOperation()
-			pending = PendingBinaryOperationInfo(firstOperand: accumulator, function: function)
-		case .Equals:
-			executePendingBinaryOperation()
+	public var program: Instructions {
+		get {
+			return currentProgram as CalculatorBrain.Instructions
+		}
+		set {
+			if let program = newValue as? [AnyObject] {
+				clearAll()
+				for op in program {
+					if let operand = op as? Double {
+						setOperand(operand)
+					} else if let symbol = op as? String {
+						doOperation(symbol)
+					}
+				}
+			}
 		}
 	}
 	
 	public var result: Double {
 		get {
 			return accumulator
+		}
+	}
+	
+	public func clearAll() {
+		accumulator = 0.0
+		pending = nil
+		currentProgram.removeAll()
+	}
+	
+	public func setOperand(_ operand: Double) {
+		accumulator = operand
+		currentProgram.append(operand as CalculatorBrain.Instructions)
+	}
+	
+	public func doOperation(_ symbol: String) {
+		currentProgram.append(symbol as CalculatorBrain.Instructions)
+		
+		switch operation[symbol]! {
+		case .Constant(let value): accumulator = value
+		case .UnaryOperation(let function):
+			executePendingBinaryOperation()
+			accumulator = function(accumulator)
+		case .BinaryOperation(let function):
+			executePendingBinaryOperation()
+			pending = PendingBinaryOperationInfo(firstOperand: accumulator, function: function)
+		case .Equals:
+			executePendingBinaryOperation()
 		}
 	}
 }

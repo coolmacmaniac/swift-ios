a9eefee96ba28ad8061285506ce98f03cd6928b6
Hans Kammler <coolmacmaniac@gmail.com>
2017-06-24 18:25:10 +0530

    Initial commit for design patterns project

---
+++ DesignPatterns/DesignPatterns/#main/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  DesignPatterns
+//
+//  Created by Sourabh on 24/06/17.
+//  Copyright © 2017 Home. All rights reserved.
+//
+
+import Foundation
+
+//`xcrun swift -version`
+print("Hello, World!")
+print("Welcome to the world of design patterns in Swift 3.1")

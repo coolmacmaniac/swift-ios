02b218399897f8219a8fa7a29078f6b3dcd29a6f
Hans Kammler <coolmacmaniac@gmail.com>
2018-03-08 00:51:02 +0530

    Basic project files for magic 8 ball app

---
+++ anyu/Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Sourabh on 08/03/18.
+//  Copyright © 2018 Home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+

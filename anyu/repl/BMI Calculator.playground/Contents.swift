446961ea3d50ff38b56c5f408c1a8d466f064751
Hans Kammler <coolmacmaniac@gmail.com>
2018-03-08 22:54:06 +0530

    Adding business logic BMI calculator

--- anyu/repl/BMI Calculator.playground/Contents.swift
+++ anyu/repl/BMI Calculator.playground/Contents.swift
@@ -2,4 +2,18 @@
 
 import UIKit
 
-var str = "Hello, playground"
+func calculateBMI(mass: Float, height: Float) -> Float {
+	return mass / powf(height, 2.0)
+}
+
+let bmi = calculateBMI(mass: 80, height: 1.75)
+
+if bmi > 30.0 {
+	print("You are obese")
+} else if bmi > 25.0 {
+	print("You are overweight")
+} else if bmi >= 18.5 {
+	print("You have normal weight")
+} else {
+	print("You are underweight")
+}
